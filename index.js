38410b08862be4302fe8f4bdcc3fa3d821d0c086
= <neymegen50@gmail.com>
2022-02-13 21:02:01 +0200

    message

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 export function finishForm() {
+  const inputPassword = document.querySelector('input');
+  inputPassword.setAttribute('type', 'password');
   const newInput = document.createElement('input');
   newInput.setAttribute('type', 'text', 'name', 'login');
   const loginForm = document.querySelector('form');
   loginForm.prepend(newInput);
-  const inputPassword = document.querySelector('.password');
-  inputPassword.setAttribute('type', 'password');
 }
