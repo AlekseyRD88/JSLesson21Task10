1b05af8801deb83742c7977b3489ffe053ce6775
= <neymegen50@gmail.com>
2022-02-13 20:51:12 +0200

    message

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 export function finishForm() {
-  const inputPassword = document.querySelector('input');
-  inputPassword.setAttribute('type', 'password');
   const newInput = document.createElement('input');
   newInput.setAttribute('name', 'login');
   const loginForm = document.querySelector('form');
   loginForm.after(newInput);
+  const inputPassword = document.querySelector('input');
+  inputPassword.setAttribute('type', 'password');
 }
