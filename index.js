d6ea33d7ac705e6bf3f550587d686034149176bc
= <neymegen50@gmail.com>
2022-02-13 20:49:37 +0200

    message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export function finishForm() {
   const inputPassword = document.querySelector('input');
   inputPassword.setAttribute('type', 'password');
   const newInput = document.createElement('input');
-  newInput.setAttribute('type', 'text', 'name', 'login');
+  newInput.setAttribute('name', 'login');
   const loginForm = document.querySelector('form');
   loginForm.after(newInput);
 }
